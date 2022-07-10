425550a8de98e13dfc7ed38a1172f2486c06b77b
Jonathan Jones <97317524+JonJones98@users.noreply.github.com>
2022-07-10 10:59:28 -0400

    Sumbiting First JSX assignment

---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div>
+          <h1>Hello Dojo!</h1>
+          <h2>Things I need to do:</h2>
+          <p>*Learn React</p>
+          <p>*Climb Mt. Everest</p>
+          <p>*Run up a Mountain</p>
+          <p>*Feed the dogs</p>
+        </div>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
